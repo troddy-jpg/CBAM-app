b4c6e84d46bb4c688d4c43061039ba57634350dd
troddy-jpg <troddy@trinity.edu>
2022-11-14 17:58:45 -0600

    Still the initial commit (woah, initializing a vue app does a LOT of stuff)

---
+++ src/views/todo.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="todo">
+        <todo_comp />
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import todo_comp from '../../src/components/todo-comp.vue'
+export default {
+    name: 'todoVue',
+    components: {
+        todo_comp,
+    }
+}
+</script>
